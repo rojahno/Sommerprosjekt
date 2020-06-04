f46253802ffdf38fa9ec8edbb74ec84bb4a964e3
rojahno <andrersu@ntnu.no>
2020-06-04 15:45:47 +0200

    small changes

---
+++ Webpage/script.js
@@ -0,0 +1,12 @@
+let emailInput  = document.getElementById("UserName");
+let passwordInput = document.getElementById("password");
+
+/*if(emailInput === undefined && passwordInput === undefined){
+    alert("dummyDum");
+}
+else{
+    alert(emailInput + passwordInput);
+}
+
+*/
+
