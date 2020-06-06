61c60afd4e7549f3835dc1acf2b17fea1fc1fb2b
rojahno <andrersu@ntnu.no>
2020-06-06 21:22:51 +0200

    Changed the css for the login page

--- Webpage/script.js
+++ Webpage/script.js
@@ -1,3 +1,12 @@
+
+// This code adds jquery to the html document it is linked with.
+/*var script = document.createElement('script');
+ 
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+-----------------------------------------------------------
+
 let emailInput = document.getElementById("UserName");
 let passwordInput = document.getElementById("password");
 
@@ -22,3 +31,15 @@ function dropDownMenu() {
         }
     }
 }
+
+/**
+ * Tried to make the load more universal, but ran into trouble with jquery. 
+ * @param {String} $loadTo - should be given in a: ('#loadto') - format
+ * @param {String} $loadFrom - should be given in a: ('dir/file #element") - format
+ */
+/*
+function loadCommonElements($loadTo, $loadFrom) {
+
+    $($loadTo).load($loadFrom);
+}
+*/
